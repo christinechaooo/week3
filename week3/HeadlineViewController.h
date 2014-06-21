2d16ce6e3eaf90ed695aa7a05d33082d879a23ac
Christine Chao <christinechaooo@gmail.com>
2014-06-21 13:23:10 -0700

    add readme

---
+++ week3/HeadlineViewController.h
@@ -0,0 +1,13 @@
+//
+//  HeadlineViewController.h
+//  week3
+//
+//  Created by Christine Chao on 6/21/14.
+//  Copyright (c) 2014 Christine Chao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HeadlineViewController : UIViewController
+
+@end
