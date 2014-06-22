ab51105f1cdb92e620977ea85e2dae513680d7d3
Christine Chao <christinechaooo@gmail.com>
2014-06-23 01:32:51 +0300

    add gestures

--- week3/HeadlineViewController.m
+++ week3/HeadlineViewController.m
@@ -12,8 +12,10 @@
 @interface HeadlineViewController ()
 
 @property (assign,nonatomic) CGPoint offset;
+@property (strong, nonatomic) UIImageView* headlineImageView;
 
 - (void)onHeadlineDrag:(UIPanGestureRecognizer *)sender;
+- (void)onTap:(UITapGestureRecognizer *)sender;
 
 @end
 
@@ -33,15 +35,33 @@
     [super viewDidLoad];
     
     UIImage *headlineImage = [UIImage imageNamed:@"headline"];
-    UIImageView *headlineImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
-    headlineImageView.image = headlineImage;
-    
-    
+    self.headlineImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 320, 568)];
+    self.headlineImageView.image = headlineImage;
+    CALayer * layerMod = [self.headlineImageView layer];
+    [layerMod setMasksToBounds:YES];
+    [layerMod setCornerRadius:4.0];
     
     UIPanGestureRecognizer *panGestureRecognizer = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(onHeadlineDrag:)];
+    UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(onTap:)];
     
-    [self.view addSubview:headlineImageView];
-    [self.view addGestureRecognizer:panGestureRecognizer];
+    self.headlineImageView.userInteractionEnabled = YES;
+    
+    UIScrollView *feedScrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 315, 320, 253)];
+    
+    UIImage *feedImg = [UIImage imageNamed:@"news"];
+    UIImageView *feedImgView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, 1444, 253)];
+    feedImgView.image = feedImg;
+    feedScrollView.contentSize = feedImgView.frame.size;
+    [feedScrollView addSubview:feedImgView];
+    
+    [self.headlineImageView addGestureRecognizer:panGestureRecognizer];
+    [self.headlineImageView addGestureRecognizer:tapGestureRecognizer];
+
+    [self.view addSubview:self.headlineImageView];
+    
+    [self.headlineImageView addSubview:feedScrollView];
+    
+    
 }
 
 - (void)didReceiveMemoryWarning
@@ -52,25 +72,61 @@
 
 - (void)onHeadlineDrag:(UIPanGestureRecognizer *)sender {
     CGPoint touchPosition = [sender locationInView:self.view];
-    
-    
-    
+    CGPoint gestureVelocity = [sender velocityInView:self.headlineImageView];
     
     if (sender.state == UIGestureRecognizerStateBegan ) {
         
-//        NSLog(@"Pan started! %f %f", touchPosition.x, touchPosition.y);
-        
-        self.offset = CGPointMake(0, touchPosition.y - self.view.center.y);
-        
+        self.offset = CGPointMake(0, touchPosition.y - self.headlineImageView.center.y);
         
     } else if (sender.state == UIGestureRecognizerStateChanged) {
-//        NSLog(@"Pan moved %f %f", touchPosition.x + self.offset.x , touchPosition.y + self.offset.y);
+//        NSLog(@"%f ", self.headlineImageView.center.y);
+        
+        if(self.headlineImageView.center.y >= 284) {
+            self.headlineImageView.center = CGPointMake(160 , touchPosition.y - self.offset.y);
+        } else {
+            self.headlineImageView.center = CGPointMake(160 , 283);
+        }
         
         
-        NSLog(@"%f %f", touchPosition.x, touchPosition.y);
-        self.view.center = CGPointMake(160 , touchPosition.y + self.offset.y);
-        
+    } else if (sender.state == UIGestureRecognizerStateEnded) {
+        if(gestureVelocity.y > 0) {
+            [UIView animateWithDuration:0.4
+                                  delay:0
+                                options:UIViewAnimationOptionCurveEaseInOut
+                             animations:^{
+                                 self.headlineImageView.center = CGPointMake(160, 808);
+                                 
+                             } completion:^(BOOL finished) {
+            
+                             }];
+        } else if(gestureVelocity.y < 0) {
+            [UIView animateWithDuration:0.4
+                                  delay:0
+                                options:UIViewAnimationOptionCurveEaseInOut
+                             animations:^{
+                                 self.headlineImageView.center = CGPointMake(160, 284);
+                                 
+                             } completion:^(BOOL finished) {
+                                 
+                             }];
+        }
     }
+}
+
+- (void)onTap:(UITapGestureRecognizer *)sender {
+    NSLog(@"tap");
+    if(self.headlineImageView.center.y == 808) {
+        [UIView animateWithDuration:0.4
+                              delay:0
+                            options:UIViewAnimationOptionCurveEaseInOut
+                         animations:^{
+                             self.headlineImageView.center = CGPointMake(160, 284);
+                             
+                         } completion:^(BOOL finished) {
+                             
+                         }];
+    }
+
 }
 
 /*
